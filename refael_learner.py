436aadbd0d44fc6d2408d21571f19d7dbe6e92a1
Itay Levinas <itaylevinas@gmail.com>
2018-09-20 16:01:53 +0300

    add xgboost + combinations

--- refael_learner.py
+++ refael_learner.py
@@ -41,11 +41,11 @@ class RefaelLearner:
     def run_ml(self):
         time = 0
         while self._database.forward_time():
-            # print("-----------------------------------    TIME " + str(time) + "    ----------------------------------")
+            print("-----------------------------------    TIME " + str(time) + "    ----------------------------------")
             time += 1
             beta_matrix, nodes_list, edges_list, labels = self._database.calc_curr_time()
             self._ml_learner.forward_time_data(beta_matrix, nodes_list, edges_list, labels)
-        self._ml_learner.run()
+            self._ml_learner.run()
 
     def run_al(self, pkl_result=False):
         if RESULT_PKL_PATH not in os.listdir("."):
